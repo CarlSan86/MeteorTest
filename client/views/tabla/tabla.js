9836dc1d513bf9511bdd0d4550f78779a65a0aeb
Carlos Sandoval Fernández <carlos.sandoval@ayp.com.mx>
2014-02-17 10:30:41 -0600

    foreach para json

--- client/views/tabla/tabla.js
+++ client/views/tabla/tabla.js
@@ -2,9 +2,13 @@ $.ajax({
     type: "GET",
     url: "http://201.151.124.53:8082/json/valores/tabla.php?etapa=2994",
     success:function(jsonData){
-    	for (var i = jsonData.length - 1; i >= 0; i--) {
-    		console.log(jsonData[i]);
-    	};
+    	$.each(jsonData, function()
+    	{
+		  $.each(this, function(k, v)
+		  {
+		    console.log(k+" "+v);
+		  });
+		});
     },
     error:function(msg) {
         alert('Error en JSON Data');
