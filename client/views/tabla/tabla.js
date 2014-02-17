108ed17e86948bb89eb8be60b3c46b9a9a6f4d1a
Carlos Sandoval Fernández <carlos.sandoval@ayp.com.mx>
2014-02-17 11:31:02 -0600

    validar de solo insertar cuando vacio

--- client/views/tabla/tabla.js
+++ client/views/tabla/tabla.js
@@ -1,18 +1,20 @@
-$.ajax({
-    type: "GET",
-    datatype: "json",
-    url: "http://201.151.124.53:8082/json/valores/tabla.php?etapa=2994",
-    success:function(jsonData){
-        str = jQuery.parseJSON(jsonData);
-		  $.each(str, function(k, v)
-		  {
-		    Capturas.insert(v);
-		  });
-    },
-    error:function(msg) {
-        alert('Error en JSON Data');
-    },
-});
+if(Capturas.find().count() === 0){
+	$.ajax({
+	    type: "GET",
+	    datatype: "json",
+	    url: "http://201.151.124.53:8082/json/valores/tabla.php?etapa=2994",
+	    success:function(jsonData){
+	        str = jQuery.parseJSON(jsonData);
+			  $.each(str, function(k, v)
+			  {
+			    Capturas.insert(v);
+			  });
+	    },
+	    error:function(msg) {
+	        alert('Error en JSON Data');
+	    },
+	});
+}
 
 $('#tabla-prods').dataTable();
 
