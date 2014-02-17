33ed3ed07ae790a7b5c06f79921a8b4e3e643c81
Carlos Sandoval Fernández <carlos.sandoval@ayp.com.mx>
2014-02-17 13:56:34 -0600

    Modificaciones a query e inserciones

--- client/views/tabla/tabla.js
+++ client/views/tabla/tabla.js
@@ -2,7 +2,7 @@ if(Capturas.find().count() === 0){
 	$.ajax({
 	    type: "GET",
 	    datatype: "json",
-	    url: "http://201.151.124.53:8082/json/valores/tabla.php?etapa=2994",
+	    url: "http://201.151.124.53:8082/json/valores/tabla.php?etapa=2994&registros=100",
 	    success:function(jsonData){
 	        str = jQuery.parseJSON(jsonData);
 			  $.each(str, function(k, v)
@@ -16,10 +16,10 @@ if(Capturas.find().count() === 0){
 	});
 }
 
-$('#tabla-prods').dataTable();
+$('table').dataTable();
 
 Template.tabla.helpers({
 	registros: function(){
-		return Capturas.find();
+		return Capturas.find().limit( 1 );
 	}
 });
