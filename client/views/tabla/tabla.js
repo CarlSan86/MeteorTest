067adbc1eafe0297899e2a72e60a5eae4aadb46b
Carlos Sandoval Fernández <carlos.sandoval@ayp.com.mx>
2014-02-17 15:36:33 -0600

    reporte reducido

--- client/views/tabla/tabla.js
+++ client/views/tabla/tabla.js
@@ -1,25 +1,25 @@
 
-	$.ajax({
-	    type: "GET",
-	    datatype: "json",
-	    url: "http://201.151.124.53:8082/json/valores/tabla.php?etapa=2994&registros=100000",
-	    success:function(jsonData){
-	        str = jQuery.parseJSON(jsonData);
-			  $.each(str, function(k, v)
-			  {
-			    Capturas.insert(v);
-			  });
-	    },
-	    error:function(msg) {
-	        alert('Error en JSON Data');
-	    },
-	});
+// 	$.ajax({
+// 	    type: "GET",
+// 	    datatype: "json",
+// 	    url: "http://201.151.124.53:8082/json/valores/tabla.php?etapa=2994&registros=100000",
+// 	    success:function(jsonData){
+// 	        str = jQuery.parseJSON(jsonData);
+// 			  $.each(str, function(k, v)
+// 			  {
+// 			    Capturas.insert(v);
+// 			  });
+// 	    },
+// 	    error:function(msg) {
+// 	        alert('Error en JSON Data');
+// 	    },
+// 	});
 
 
-$('table').dataTable();
+// $('table').dataTable();
 
 Template.tabla.helpers({
 	registros: function(){
-		return Capturas.find().limit( 1 );
+		return Capturas.find({tipoValor:"Inventario",valorCapturado:"5.00"});
 	}
 });
