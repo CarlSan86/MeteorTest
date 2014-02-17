62659ff0928822f5f55e2784b9be309eca126322
Carlos Sandoval Fernández <carlos.sandoval@ayp.com.mx>
2014-02-17 09:32:39 -0600

    Inicio de llamada AJAX a JSON para fixture

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,21 +1,20 @@
 if(Capturas.find().count() === 0){
 	
-	Capturas.insert({
-		id: "1",
-		usuario: "Carlos Sandoval",
-		valor: "Precio",
-		registro: "4.5"
-	});
-	Capturas.insert({
-		id: "2",
-		usuario: "Alan Trejo",
-		valor: "Inventario",
-		registro: "5"
-	});
-	Capturas.insert({
-		id: "3",
-		usuario: "Angel Villalobos",
-		valor: "Precio",
-		registro: "7.5"
-	});
+	$.ajax({
+        type: "GET",
+        url: "201.151.124.53:8082/json/valores/tabla.php?etapa=2994",
+        success:function(jsonData){
+            console.log(jsonData);
+        },
+        error:function(msg) {
+            alert('Error en JSON Data');
+        },
+    });
+
+	// Capturas.insert({
+	// 	id: "1",
+	// 	usuario: "Carlos Sandoval",
+	// 	valor: "Precio",
+	// 	registro: "4.5"
+	// });
 }
