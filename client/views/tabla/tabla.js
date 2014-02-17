bb58f94b7eeab6d8ceb03d77973f3faf6812d638
Carlos Sandoval Fernández <carlos.sandoval@ayp.com.mx>
2014-02-17 09:47:12 -0600

    ajax a tabla

--- client/views/tabla/tabla.js
+++ client/views/tabla/tabla.js
@@ -1,3 +1,14 @@
+$.ajax({
+    type: "GET",
+    url: "201.151.124.53:8082/json/valores/tabla.php?etapa=2994",
+    success:function(jsonData){
+        console.log(jsonData);
+    },
+    error:function(msg) {
+        alert('Error en JSON Data');
+    },
+});
+
 Template.tabla.helpers({
 	registros: function(){
 		return Capturas.find();
