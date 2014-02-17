667d8681f646b1f80e0f8cb5fc0fb947a6712955
Carlos Sandoval Fernández <carlos.sandoval@ayp.com.mx>
2014-02-17 11:01:55 -0600

    borrar coleccion antes de insertar

--- client/views/tabla/tabla.js
+++ client/views/tabla/tabla.js
@@ -3,6 +3,7 @@ $.ajax({
     datatype: "json",
     url: "http://201.151.124.53:8082/json/valores/tabla.php?etapa=2994",
     success:function(jsonData){
+    	Capturas.remove({});
         str = jQuery.parseJSON(jsonData);
 		  $.each(str, function(k, v)
 		  {
