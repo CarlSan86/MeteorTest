8d87482bcc5ce9f3d6fe89dc79f5deb89f5b5c94
Carlos Sandoval Fernández <carlos.sandoval@ayp.com.mx>
2014-02-17 11:00:04 -0600

    llenado de tabla con registros de vista

--- client/views/tabla/tabla.js
+++ client/views/tabla/tabla.js
@@ -1,14 +1,13 @@
 $.ajax({
     type: "GET",
+    datatype: "json",
     url: "http://201.151.124.53:8082/json/valores/tabla.php?etapa=2994",
     success:function(jsonData){
-    	$.each(jsonData, function()
-    	{
-		  $.each(this, function(k, v)
+        str = jQuery.parseJSON(jsonData);
+		  $.each(str, function(k, v)
 		  {
-		    console.log(k+" "+v);
+		    Capturas.insert(v);
 		  });
-		});
     },
     error:function(msg) {
         alert('Error en JSON Data');
